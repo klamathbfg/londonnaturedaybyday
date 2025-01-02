743415b9242b9e08b5ab8c2b30c263d2b2388d97
Stephen Renouf <tom@londonnaturedaybyday.com>
2025-01-02 23:59:14 +0000

    Updates to finish sorting and preview mode

--- articles/urls.py
+++ articles/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.ArticleGroupsView.as_view(), name="articlegroups"),
     path("<int:pk>/", views.ArticleView.as_view(), name="articlecontents"),
+    path("preview/<int:pk>/", views.ArticlePreview.as_view(), name="articlecontents"),
     path("grouping/<int:pk>/", views.ArticleGroupContentsView.as_view(), name="articlegroupcontents"),
     path("today", views.Today.as_view(), name="today"),
 ]
--- articles/views.py
+++ articles/views.py
@@ -37,10 +37,23 @@ class ArticleView(generic.ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['latest_article_group_list'] = Article_Group.objects.filter(pub_date__lte=timezone.now()).order_by("sort_order")[ :20]
-        article_sections = Article_Section.objects.filter(pub_date__lte=timezone.now(),article_section_link__article_id=self.kwargs['pk'])[:20]
-        """article_sections = Article_Section.objects.filter(pub_date__lte=timezone.now(),
-                                                          article_section_link__article_id=self.kwargs['pk']).annotate(section_sort_order=('article_section_link__sort_order')).order_by('section_sort_order')[:20]"""
-        context['latest_article_sections_list'] = article_sections
+        article_section_links = Article_Section_Link.objects.filter(article_section__pub_date__lte=timezone.now(),article_id=self.kwargs['pk']).order_by("sort_order")[:20]
+        context['latest_article_sections_list'] = article_section_links
+        return context   
+
+class ArticlePreview(generic.ListView):
+    model=Article
+    template_name = "articles/article.html"
+    context_object_name = "article_contents"
+
+    def get_queryset(self):
+        return Article.objects.filter(pk=self.kwargs['pk'])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['latest_article_group_list'] = Article_Group.objects.filter(pub_date__lte=timezone.now()).order_by("sort_order")[ :20]
+        article_section_links = Article_Section_Link.objects.filter(article_id=self.kwargs['pk']).order_by("sort_order")[:20]
+        context['latest_article_sections_list'] = article_section_links
         return context    
     
 class Today(generic.ListView):
